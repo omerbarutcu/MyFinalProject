39cc98c8cd12640792aa8d03760fd7483f670b86
ömer barutçu <omerbrtc@hotmail.com>
2021-04-01 17:25:28 +0300

    01.04.2021 10.ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,6 +10,6 @@ namespace Business.Constants
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem Bakımda";
-        public static string ProductsListed="Ürünler listelendi";
+        public static string ProductsListed="Ürünler listelendi"; 
     }
 }
