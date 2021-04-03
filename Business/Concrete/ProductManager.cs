2337d46c4ccf3210de76dd8db4cbb64ea65e937a
ömer barutçu <omerbrtc@hotmail.com>
2021-04-04 00:34:17 +0300

    04.04.2021 11.ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -35,7 +35,7 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //İş kodları
-            if (DateTime.Now.Hour==16)
+            if (DateTime.Now.Hour==10)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
